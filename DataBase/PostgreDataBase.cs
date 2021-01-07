f44572470bd82b7d9aaab5ac599b35e91b088a92
rogurotus <rogurotus@gmail.com>
2021-01-07 11:57:36 +0700

    Добавил модели в БД

--- DataBase/PostgreDataBase.cs
+++ DataBase/PostgreDataBase.cs
@@ -1,9 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using Server.Models;
 
 namespace Server.DataBase
 {
     public class PostgreDataBase : DbContext
     {
+        public DbSet<Ticket> tikets {get; set;}
+
+        public DbSet<TrafficLight> traffic_lights {get; set;}
+
+        public DbSet<TicketTrafficLight> ticket_traffic_lights {get; set;}
+        public DbSet<OtherTicket> other_tickets {get; set;}
+        
         public PostgreDataBase (DbContextOptions<PostgreDataBase> options)
             : base(options) 
         {
