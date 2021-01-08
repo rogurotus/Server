26c6efc56b3d65cce82996e91dd6a94cd985995f
rogurotus <rogurotus@gmail.com>
2021-01-08 15:24:02 +0700

    Исправил аннотации

--- Models/TicketTrafficLight.cs
+++ Models/TicketTrafficLight.cs
@@ -6,10 +6,11 @@ namespace Server.Models
     [Table("ticket_traffic_light")]
     public class TicketTrafficLight
     {
-        [Key, ForeignKey("id")]
+        [Key]
+        public int ticket_id {get; set;}
+        [ForeignKey("tiket_id")]
         public Ticket ticket {get; set;}
 
-        [ForeignKey("id")]
         public TrafficLight traffic_light {get; set;}
 
         public string description {get; set;}
