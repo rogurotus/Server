295416a2df4d9c3abefceced177627468859fe56
rogurotus <rogurotus@gmail.com>
2021-01-16 16:22:30 +0700

    Добавил аутентификацию на куках

--- Controllers/WebUserController.cs
+++ Controllers/WebUserController.cs
@@ -11,6 +11,9 @@ using Server.DataBase;
 using System.Security.Cryptography;
 using System.Text;
 using Server.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Server.Controllers
 {
@@ -34,7 +37,7 @@ namespace Server.Controllers
             return BitConverter.ToString(hashData).Replace("-","");
         }
 
-        [HttpPost]
+        [HttpPost("Login")]
         public async Task<ActionResult<SimpleResponse>> Login(WebUser user)
         {
             string hash_pass = hasher(user.pass);
@@ -45,12 +48,13 @@ namespace Server.Controllers
             
             if(user != null)
             {
+                await Authenticate(user_db.email);
                 return new SimpleResponse{message = "Вход выполнен успешно"};
             }
             return new SimpleResponse{error = "Пользователь с таким логином и паролем не найден"};
         }
 
-        [HttpPost]
+        [HttpPost("Create")]
         public async Task<ActionResult<SimpleResponse>> CreateAccount(WebUser user)
         {
             string hash_pass = hasher(user.pass);
@@ -59,13 +63,40 @@ namespace Server.Controllers
                 .Where(u => u.login == user.login || u.email == user.email)
                 .FirstOrDefaultAsync();
             
-            if(user == null)
+            if(user_db == null)
             {
-                await _db.users.AddAsync(new WebUser{login = user.login, pass = hash_pass, email = user.email});
+                await _db.users.AddAsync(
+                    new WebUser{login = user.login, pass = hash_pass, email = user.email});
                 await _db.SaveChangesAsync();
+                await Authenticate(user.email);
                 return new SimpleResponse{message = "Аккаунт создан успешно"};
             }
             return new SimpleResponse{error = "Пользователь с таким логином или почтой уже существует"};
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            ClaimsIdentity id = new ClaimsIdentity(
+                claims, "ApplicationCookie", 
+                ClaimsIdentity.DefaultNameClaimType, 
+                ClaimsIdentity.DefaultRoleClaimType
+            );
+            await HttpContext
+                .SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme, 
+                    new ClaimsPrincipal(id)
+                );
+        }
+ 
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/Authorization/Entrance");
         }
     }
 }
