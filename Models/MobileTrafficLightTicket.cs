673b3474a3ab4944a09b60ae7def311b59b247be
rogurotus <rogurotus@gmail.com>
2021-01-20 23:30:57 +0700

    Уточнил возможные заявки

--- Models/MobileTrafficLightTicket.cs
+++ Models/MobileTrafficLightTicket.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
+
 namespace Server.Models
 {
-    public class MobileTicket
+    public class MobileTrafficLightTicket
     {
         public int traffic_light_id {get; set;}
-        public string user_id {get; set;}
+        public string user_token {get; set;}
         public string description {get;set;}
     }
 }
