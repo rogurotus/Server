e65814bd5cd60f65ec511ea880ad5a3725032ff2
rogurotus <rogurotus@gmail.com>
2021-01-20 23:25:07 +0700

    Добавил Тип заявок

---
+++ Models/TicketType.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
+namespace Server.Models
+{
+    [Table("ticket_type")]
+    public class TicketType
+    {
+        [Key]
+        public int id {get; set;}
+
+        public string name {get; set;}
+
+        public string description {get; set;}
+
+        public int image_id {get; set;}
+
+    }
+}
