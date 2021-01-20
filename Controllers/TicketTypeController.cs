afe7e80a2c58212d8dc638d5ec80e6ab7a67101d
rogurotus <rogurotus@gmail.com>
2021-01-20 23:33:45 +0700

    добавил рест запросы на типы заявок и светофоры

---
+++ Controllers/TicketTypeController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Docker.Models;
+using Microsoft.EntityFrameworkCore;
+using Server.DataBase;
+using System.Security.Cryptography;
+using System.Text;
+using Server.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketTypeController : ControllerBase
+    {
+        private PostgreDataBase _db;
+
+        public TicketTypeController(PostgreDataBase db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<TicketType>>> GetTypes()
+        {
+            return await _db.tiket_types.ToListAsync();
+        }
+    }
+}
---
+++ Controllers/TrafficLightController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Docker.Models;
+using Microsoft.EntityFrameworkCore;
+using Server.DataBase;
+using System.Security.Cryptography;
+using System.Text;
+using Server.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrafficLightController : ControllerBase
+    {
+        private PostgreDataBase _db;
+
+        public TrafficLightController(PostgreDataBase db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TrafficLight>>> GetTrafficLight()
+        {
+            return await _db.traffic_lights.ToListAsync();
+        }
+    }
+}
